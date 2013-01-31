f90788d8e5339ac886795ef33a2e433f397df58c
Jake <dragonjts@gmail.com>
2013-01-31 15:57:36 -0500

    Slideshow
    Modified slideshow.js to meet requirments
    Added content to index.html

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,12 +3,33 @@ File: slideshow.js
 Author: Jake Marshall
 Site: Jake's Portfolio
 Description: Changes the featured work image on the homepage when clicked
-*/
-var step = 1;
+ */
+var step = 3;
 function changeImage() {
 	if (step < 3)
 		step++;
 	else
 		step = 1;
-	document.getElementById("slideviewer").src="img\\screenshot"+step+".png";
+	document.getElementById("slideviewer").src = "img\\screenshot" + step
+			+ ".png";
+	setTimeout("changeImage()", 3000);
+}
+changeImage();
+
+function openLink() {
+	if (step == 1)
+		window
+				.open(
+						"http://www.windowsphone.com/en-us/store/app/snakeit/d829cac5-9e8a-4245-9ebd-0d3cc3b06bc7",
+						'_blank');
+	else if (step == 2)
+		window
+				.open(
+						"http://www.windowsphone.com/en-us/store/app/asteroid-field/a33ee253-4561-4a3a-bf4a-de68bdded002",
+						'_blank');
+	else if (step == 3)
+		window
+				.open(
+						"http://www.windowsphone.com/en-us/store/app/zombie-runner/2519d89c-8e74-4597-8047-9081b69c03c7",
+						'_blank');
 }
