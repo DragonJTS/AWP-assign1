b457f4e8632a58d20b70dbc901008facb58175f9
Jake <dragonjts@gmail.com>
2013-01-31 21:46:45 -0500

    Sitemap
    Added sitemap pag
    Modified footer js to add sitemap link on other pages
    changed services content

--- js/footer.js
+++ js/footer.js
@@ -3,5 +3,7 @@ File: footer.js
 Author: Jake Marshall
 Site: Jake's Portfolio
 Description: output the copyright footer, is shorter then this header
-*/
+ */
+if (document.title != "Site Map")
+	document.write("<a href=\"sitemap.html\">Sitemap</a><br>");
 document.write("© Jake Marshall, 2013");
